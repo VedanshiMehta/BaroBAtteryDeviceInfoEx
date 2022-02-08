d8552b71fbff15e5872e99a93cf67e7761a08e06
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-02-08 17:29:24 +0530

    Add project files.

---
+++ BaroBAtteryDeviceInfoEx/BaroDemo.cs
@@ -0,0 +1,73 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace BaroBAtteryDeviceInfoEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    class BaroDemo : Activity
+    {
+
+        private Button start;
+        private Button stop;
+        private TextView textV;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.baroDemo);
+            UIReference();
+            UIClickEvent();
+        }
+
+        private void UIClickEvent()
+        {
+            start.Click += Start_Click;
+            stop.Click += Stop_Click;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (Barometer.IsMonitoring)
+            {
+                Barometer.ReadingChanged -= Barometer_ReadingChanged;
+                Barometer.Stop();
+            }
+        }
+
+
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (!Barometer.IsMonitoring)
+            {
+                Barometer.ReadingChanged += Barometer_ReadingChanged;
+                Barometer.Start(SensorSpeed.UI);
+
+            }
+        }
+
+        private void Barometer_ReadingChanged(object sender, BarometerChangedEventArgs e)
+        {
+            textV.Text = $"Pressure: {e.Reading.PressureInHectopascals} hectopascals";
+        }
+
+
+        private void UIReference()
+        {
+            start = FindViewById<Button>(Resource.Id.buttonStartBD);
+            stop = FindViewById<Button>(Resource.Id.buttonStopBD);
+            textV = FindViewById<TextView>(Resource.Id.textViewBD);
+
+        }
+    }
+}
+   
---
+++ BaroBAtteryDeviceInfoEx/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace BaroBAtteryDeviceInfoEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button myBarometer;
+        Button myBattery;
+        Button myDeviceInfo;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+            myBarometer = FindViewById<Button>(Resource.Id.button1);
+            myBattery = FindViewById<Button>(Resource.Id.button2);
+            myDeviceInfo = FindViewById<Button>(Resource.Id.button3);
+
+
+            myBarometer.Click += MyBarometer_Click;
+            myBattery.Click += MyBattery_Click;
+            myDeviceInfo.Click += MyDeviceInfo_Click;
+        }
+
+        private void MyDeviceInfo_Click(object sender, System.EventArgs e)
+        {
+            Intent k = new Intent(Application.Context, typeof(DeviceInfoEx));
+            StartActivity(k);
+        }
+
+        private void MyBattery_Click(object sender, System.EventArgs e)
+        {
+
+            Intent j = new Intent(Application.Context, typeof(BatteryDemo));
+            StartActivity(j);
+        }
+
+        private void MyBarometer_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(Application.Context, typeof(BaroDemo));
+            StartActivity(i);
+        }
+
+        private void MyPropertyDemo_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
